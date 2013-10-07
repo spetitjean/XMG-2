f1e4e2a9d78248819f3e87f86cfdf19f630d5dc5
Denys Duchier <denys.duchier@univ-orleans.fr>
2013-10-07 14:25:27 +0200

    get list of commands from xmg.ini

--- core/python/xmg/command/__init__.py
+++ core/python/xmg/command/__init__.py
@@ -4,6 +4,6 @@ parser = argparse.ArgumentParser(description="XMG metagrammar compiler")
 subparsers = parser.add_subparsers()
 
 def load_commands():
-    import xmg.command.compile
-    import xmg.command.gui
-    import xmg.command.install
+    import xmg, importlib
+    for name in xmg.config['COMMANDS']['commands'].strip().split():
+        importlib.import_module("xmg.command.%s" % name)
