785a5fa236be0f2950ff3cad1957ddf42497eacf
Denys Duchier <denys.duchier@univ-orleans.fr>
2013-10-07 17:58:22 +0200

    move commands compile and gui to new core contribution

---
+++ contributions/core/commands/startcontrib/__init__.py
@@ -0,0 +1,21 @@
+from xmg.command import subparsers
+
+#==============================================================================
+# xmg startcontrib NAME
+#==============================================================================
+
+def handler_xmg_startcontrib(args):
+    import os, os.path
+    name = args.name
+    if os.path.exists(args.name):
+        raise RuntimeError("%s already exists" % name)
+    path = os.path.realpath(name)
+    dotxmg = os.path.join(path, ".xmg")
+    os.makedirs(dotxmg)
+    typepath = os.path.join(dotxmg, "type")
+    os.symlink('CONTRIBUTION', typepath)
+
+cmd = subparser.add_parser(
+    "startcontrib", description="start a new contribution")
+cmd.add_argument("name", metavar="NAME")
+cmd.set_defaults(handler=handler_xmg_startcontrib)
