58dd517caf02133155cf2e2fea14595cf29e3b9a
Denys Duchier <denys.duchier@univ-orleans.fr>
2013-10-08 21:16:22 +0200

    new startbrick

---
+++ contributions/core/commands/startbrick/__init__.py
@@ -0,0 +1,31 @@
+from xmg.command import subparsers
+
+#==============================================================================
+# xmg startbrick NAME
+#==============================================================================
+
+LANGDEF = """
+"""
+
+# the function implementing the command
+def handler_xmg_startbrick(args):
+    import os, os.path
+    if not (os.path.islink(".xmg/type") and os.readlink(".xmg/type")=="CONTRIBUTION"):
+        raise RuntimeError("startbrick must be invoked at the top-level of a contribution")
+    name = args.name
+    path = os.path.join("bricks", name)
+    if os.path.lexists(path):
+        raise RuntimeError("%s exists already" % path)
+    os.makedirs(path, exist_ok=True)
+    os.makedirs(os.path.join(path, "compiler"))
+    os.makedirs(os.path.join(path, "yaplib"))
+    with open(os.path.join(path, "lang.def")) as f:
+        f.write(LANGDEF % {"name": name})
+
+# create the xmg subcommand
+cmd = subparsers.add_parser(
+    "startbrick", description="start a new language brick")
+cmd.set_defaults(handler=handler_xmg_startbrick)
+
+# add options and arguments here
+cmd.add_argument("name", metavar="NAME")
