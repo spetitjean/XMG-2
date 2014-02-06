28ae6bbf80fe59e81e884d55da9d3410d18f2189
Denys Duchier <denys.duchier@univ-orleans.fr>
2014-02-06 17:14:45 +0100

    more general BrickSpec

--- contributions/core/commands/build/BrickSpec.py
+++ contributions/core/commands/build/BrickSpec.py
@@ -1,46 +1,18 @@
-class BrickSpec:
-
-    def __init__(self, name, plugs):
-        import re
-        name_regex = re.compile(r"^([A-Za-z][A-Za-z0-9]*)(.*)$")
-        m = name_regex.match(name)
-        if not m:
-            raise RuntimeError("unparseable identifier in compiler.yml: %s" % name)
-        self.name = re.sub("[^A-Za-z0-9]", "_", name)
-        self.brick_name = m.group(1)
-        self.plugs = plugs
-        self.params = []
-        if "_" in plugs:
-            self.params = plugs["_"]
-            del plugs["_"]
-
-    def deref_plugs(self, table):
-        plugs = {}
-        for k,v in self.plugs.items():
-            brs = []
-            for id in v.strip().split():
-                br = table.get(id, None)
-                if br is None:
-                    br = BrickSpec(id, {})
-                    table[id] = br
-                brs.append(br)
-            plugs[k] = brs
-        self.plugs = plugs
-
-    def create_brick(self):
-        from xmg.compgen.Brick import Brick
-        # prefix voir si self.name != self.brick_name
-        self.brick = Brick(self.brick_name, prefix=self.name)
-        
+from xmg.compgen.BrickSpec import BrickSpec
 
 def yaml_to_specs(desc):
     table = {}
     for name, plugs in desc.items():
-        table[name] = BrickSpec(name, plugs)
+        for bs in BrickSpec.make(name, plugs):
+            if bs.name in table:
+                raise Exception("BrickSpec collision: %s" % br.name)
+            table[bs.name] = bs
     for spec in tuple(table.values()):
         spec.deref_plugs(table)
     return table
 
+# BUG!!!
+# this should now return a set of pairs (brick,symbol)
 def determine_axioms(table):
     used = set()
     for s in table.values():
@@ -54,6 +26,6 @@ def build_and_connect(table):
     for b in table.values():
         b.create_brick()
     for b in table.values():
-        for k,v in b.plugs.items():
-            for b2 in v:
-                b.brick.connect(k, b2.brick)
+        for k,plugs in b.plugs.items():
+            for plug in plugs:
+                b.brick.connect(k, plug.brickspec.brick, plug.symbol_name)
