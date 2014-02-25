e181ee69df29acc1e89913dd509d30ce615c8938
Denys Duchier <denys.duchier@univ-orleans.fr>
2014-02-25 17:53:17 +0100

    better stardim._Constraint plug

--- contributions/compat/bricks/stardim/pylib/brickspec.py
+++ contributions/compat/bricks/stardim/pylib/brickspec.py
@@ -1,12 +1,17 @@
-from xmg.compgen.BrickSpec import BrickSpec
+from xmg.compgen.BrickSpec import BrickSpec, Plug
 
 class StardimBrickSpec(BrickSpec):
 
     def __init__(self, name, brick_name, plugs):
         self.proxy = plugs["proxy"]
         del plugs["proxy"]
-        plugs["_Constraint"]=self.proxy+"._Extern"
         super().__init__(name, brick_name, plugs)
+
+    def deref_plugs(self, table):
+        proxy = table[self.proxy]
+        ctrl = proxy.control_name
+        self.plugs["_Constraint"] = Plug(ctrl + "._Stmt")
+        super().deref_plugs(table)
 
     def init_brick(self, brick):
         brick._text = """
