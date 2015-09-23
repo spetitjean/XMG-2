5c081ee8caa9e1c40df9eda1ad6631c3ffda420c
Simon Petitjean <petitjean.sim@gmail.com>
2015-09-23 16:11:15 +0200

    added reinstall_synframe script

---
+++ reinstall_synframe.sh
@@ -0,0 +1,11 @@
+#! /bin/bash
+
+rm -rf .install
+xmg bootstrap                      || exit 1
+cd contributions                   || exit 1
+xmg install core                   || exit 1
+xmg install frame                  || exit 1
+xmg install compat                 || exit 1
+xmg install synframeCompiler       || exit 1
+cd synframeCompiler/compilers/synframe
+xmg build                          || exit 1
