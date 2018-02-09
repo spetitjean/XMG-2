674d2ed446274230cb4eccc0cbdf664917f62511
Simon Petitjean <petitjean.sim@gmail.com>
2018-02-09 16:14:11 +0100

    Fixed script synframe

--- install_synframe.sh
+++ install_synframe.sh
@@ -1,7 +1,8 @@
 #! /bin/bash
 cd contributions                   || exit 1
 xmg install core                   || exit 1
-xmg install frame                  || exit 1
+xmg install treemg                 || exit 1
+xmg install framemg                || exit 1
 xmg install compat                 || exit 1
 xmg install synframeCompiler       || exit 1
 cd synframeCompiler/compilers/synframe
