6833b083d652f0a7853fc8beccbecf9aa5e2196a
Simon Petitjean <petitjean.sim@gmail.com>
2018-06-06 16:01:32 +0200

    Added scripts for compilers tf and framelp

--- install_framelp.sh
+++ install_framelp.sh
@@ -1,12 +1,9 @@
 #! /bin/bash
-
-rm -rf .install
-xmg bootstrap                      || exit 1
 cd contributions                   || exit 1
 xmg install core                   || exit 1
-xmg install treemg                 || exit 1
+xmg install morphmg                || exit 1
 xmg install framemg                || exit 1
 xmg install compat                 || exit 1
-xmg install synframeCompiler       || exit 1
-cd synframeCompiler/compilers/synframe
+xmg install framelpcompiler        || exit 1
+cd framelpcompiler/compilers/framelp
 xmg build                          || exit 1
