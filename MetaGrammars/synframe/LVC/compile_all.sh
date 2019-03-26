3c6aa4f20dcab8ab048e2e82be0a81721e940091
Simon Petitjean <petitjean.sim@gmail.com>
2019-03-26 14:38:25 +0100

    Documentation for LVC grammar

---
+++ MetaGrammars/synframe/LVC/compile_all.sh
@@ -0,0 +1,4 @@
+xmg compile synframe lvc-stehen-syn.mg --force --more
+xmg compile synframe lvc-stehen-frame.mg --force --more
+xmg compile lex lvc-stehen-lex.mg --force
+xmg compile mph lvc-stehen-mph.mg --force
