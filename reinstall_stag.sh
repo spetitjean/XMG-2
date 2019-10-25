a7b635def19830c1d6a9d59687f6a6dab74b5050
Simon Petitjean <petitjean.sim@gmail.com>
2019-10-25 11:10:23 +0200

    Added example mg for STAG

---
+++ reinstall_stag.sh
@@ -0,0 +1,11 @@
+#! /bin/bash
+
+rm -rf .install
+xmg bootstrap                      || exit 1
+cd contributions                   || exit 1
+xmg install core                   || exit 1
+xmg install treemg                 || exit 1
+xmg install compat                 || exit 1
+xmg install STAGCompiler         || exit 1
+cd STAGCompiler/compilers/stag
+xmg build                          || exit 1
